bcefaa1d2aa34479cfdb896f9ad11eb2cc861b56
Leonardo <leonardohp@utdelacosta.edu.mx>
2024-12-07 14:50:57 -0700

    Plantilla base

---
+++ src/test/java/edu/mx/utdelacosta/BackendJava01ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mx.utdelacosta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJava01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
